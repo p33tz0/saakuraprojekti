4c34dcff4f3b85c0ac8134a3b118724a54657b13
Petrus Savolainen <petrus.savolainen@gmail.com>
2022-03-09 16:06:54 +0200

    Tabulating

--- send_mail_demo.py
+++ send_mail_demo.py
@@ -4,6 +4,7 @@ import yagmail
 import psycopg2
 from config import config
 import requests
+from tabulate import tabulate
 
 def send_mail():
 
@@ -13,22 +14,34 @@ def send_mail():
         params = config()
         con = psycopg2.connect(**params)
         cur = con.cursor()
-        lasku = "UPDATE saakurataulu SET totalminutes=(DATE_PART('day', enddate::timestamp - startdate::timestamp) * 24 + DATE_PART('hour', enddate::timestamp - startdate::timestamp)) * 60 + DATE_PART('minute', enddate::timestamp - startdate::timestamp);"
-        SQL = 'SELECT name, startdate, enddate, project, description, weatherdescription, weathertemp, totalminutes FROM public.saakurataulu'
-        cur.execute(lasku)
+        #lasku = "UPDATE saakurataulu SET totalminutes=(DATE_PART('day', enddate::timestamp - startdate::timestamp) * 24 + DATE_PART('hour', enddate::timestamp - startdate::timestamp)) * 60 + DATE_PART('minute', enddate::timestamp - startdate::timestamp);"
+        #lasku2 = "UPDATE saakurataulu SET hoursdecimal = totalminutes / 60;"
+        SQL = 'SELECT name as Nimi, startdate, enddate, project, description, weatherdescription, weathertemp FROM public.saakurataulu'
+        #cur.execute(lasku)
+        #cur.execute(lasku2)
         cur.execute(SQL)
         con.commit()
         colnames = [desc[0] for desc in cur.description]
         print(colnames)
         row = cur.fetchone()
+        lista = []
+        listauusi = {}
+        #dicts = {}
         while row is not None:
             indexi = 0
+            ind = 0
+            listasis = []
             for i in row:
-                print(f"{i}  {colnames[indexi]}")
-                indexi += 1
-            row = cur.fetchone()
+                jorma = f"{i}"
+                listasis.append(jorma)
 
-        
+
+                
+                indexi += 1
+            lista.append(listasis)
+            row = cur.fetchone()
+        headerit = ["Nimi","Aloitusaika", "Lopetusaika", "Projekti", "Projektikuvaus", "Sää", "Lämpötila"]
+        print(tabulate(lista, headers=headerit, tablefmt="pretty"))
         con.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
