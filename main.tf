a3a7acde6daa9b021e46b0215a0e3b229d38febc
katariina <katariina.vakkuri@awacademy.training>
2022-03-10 12:06:03 +0200

    tf update

--- main.tf
+++ main.tf
@@ -12,11 +12,8 @@ provider "azurerm" {
 }
 # RG create
 resource "azurerm_resource_group" "example" {
-  name     = "saakuraprojekti-rg"
+  name     = "saakuraprojektin-rg"
   location = "westeurope"
-     lifecycle {
-    prevent_destroy = true
-  } 
 }
 # postgreSQL server create
 resource "azurerm_postgresql_server" "psql" {
@@ -51,50 +48,21 @@ resource "azurerm_postgresql_database" "db" {
     prevent_destroy = true
   }
 }
-# storage account create
-resource "azurerm_storage_account" "example" {
-  name                     = "saakurasa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-     lifecycle {
-    prevent_destroy = true
-  }
-}
-#Create vnet
+# Create vnet
 resource "azurerm_virtual_network" "example" {
   name                = "saakuravnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-     lifecycle {
-    prevent_destroy = true
-  }
 }
-
+# create subnet
 resource "azurerm_subnet" "example" {
   name                 = "saakruasub"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
-     lifecycle {
-    prevent_destroy = true
-  }
 }
-
-resource "azurerm_public_ip" "example" {
-  name                = "saakura-publicip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  domain_name_label   = azurerm_resource_group.example.name
-
-  tags = {
-    environment = "staging"
-  }
-}
-
+# Create lb
 resource "azurerm_lb" "example" {
   name                = "saakura-lb"
   location            = azurerm_resource_group.example.location
@@ -130,4 +98,98 @@ resource "azurerm_lb_probe" "example" {
   protocol            = "Http"
   request_path        = "/health"
   port                = 8080
+}
+# Create nic
+resource "azurerm_network_interface" "example" {
+  name                = "saakuranic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "saakruaiipee"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example2.id
+  }
+}
+# Create strgaccount
+resource "azurerm_storage_account" "example" {
+  name                     = "saakurastrgacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+# Create strgcontainer
+resource "azurerm_storage_container" "example" {
+  name                  = "saakuracontt"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+# Create managed disk
+ resource "azurerm_managed_disk" "example" {
+   name                 = "datadisk_existing"
+   location             = azurerm_resource_group.example.location
+   resource_group_name  = azurerm_resource_group.example.name
+   storage_account_type = "Standard_LRS"
+   create_option        = "Empty"
+   disk_size_gb         = "1023"
+ }
+# Create availability set
+resource "azurerm_availability_set" "example" {
+  name                = "saakuraavailabilitysetti"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  tags = {
+    environment = "Production"
+  }
+}
+# Get a Static Public IP
+resource "azurerm_public_ip" "example" {
+  name = "linux-xxxxx-vm-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+# Get a Static Public IP
+resource "azurerm_public_ip" "example2" {
+  name = "linux-yyyyyy-vm-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+# Data template Bash bootstrapping file
+data "template_file" "linux-vm-cloud-init" {
+  template = file("install-programs.sh")
+}
+# Create Linux VM with web server
+resource "azurerm_linux_virtual_machine" "web-linux-vm" {
+  name = "linux-saakura-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  availability_set_id   = azurerm_availability_set.example.id
+  size                  = "Standard_B2s"
+  
+  source_image_reference {
+     publisher = "Canonical"
+     offer     = "0001-com-ubuntu-server-focal"
+     sku       = "20_04-lts-gen2"
+     version   = "latest"
+   }
+  os_disk {
+   name = "linux-saakura-vm-os-disk"
+   caching              = "ReadWrite"
+   storage_account_type = "Standard_LRS"
+  }
+  computer_name  = "hostname"
+  admin_username = "testadmin"
+  admin_password = "Password1234!"
+  disable_password_authentication = false
+  custom_data    = base64encode(data.template_file.linux-vm-cloud-init.rendered)
 }
